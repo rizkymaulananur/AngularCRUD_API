20e8cc06447b026054acd5b99bcdde88d6d3da21
Rizky Maulana <mk.rizky.maulana6@pertamina.com>
2022-12-02 07:27:16 +0700

    first commit

---
+++ AngularCRUD_API/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularCRUD_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Player>>> GetPlayers()
+        {
+            return new List<Player>
+            {
+                new Player
+                {
+                    username = "rizkymaulana",
+                    firstName = "Rizky",
+                    lastName = "Maulana"
+                }
+            };
+        }
+    }
+}
---
+++ AngularCRUD_API/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace AngularCRUD_API
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public string username { get; set; } = string.Empty;
+        public string firstName { get; set; } = string.Empty;
+        public string lastName { get; set; } = string.Empty;
+    }
+}
